5a9c666b3351deeddfb4fad0695b3c8dd13fcd40
Sergeev Mikhail <sergeev-michael@mail.ru>
2023-12-08 16:35:59 +0600

    Create project home_work66. Intasll Router, MaterialUI. Create .editorconfig file

---
+++ src/Navbar/Navbar.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import {Toolbar} from "@mui/material";
+import ReactIcon from '../images/ic-react.png';
+import PlusIcon from '../images/ic-plus.png';
+import MuiIcon from '../images/ic-mui.png';
+
+const Navbar = () => {
+    return (
+        <>
+            <AppBar >
+                <Toolbar sx={{backgroundColor: '#90caf9'}}>
+                    <Box sx={{display: 'flex',
+                        alignItems: 'center',
+                        gap: 2,
+                    paddingTop: '10px',
+                        paddingBottom: '10px'}}>
+                        <Box component="img"
+                             sx={{width: 50, height: 50}}
+                             src={ReactIcon}
+                        />
+                        <Box component="img"
+                             sx={{width: 25, height: 25}}
+                             src={PlusIcon}
+                        />
+                        <Box component="img"
+                             sx={{width: 50, height: 50}}
+                             src={MuiIcon}
+                        />
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </>
+    );
+};
+
+export default Navbar;
